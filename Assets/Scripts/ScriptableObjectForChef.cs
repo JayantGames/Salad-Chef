2be66c8f00e68e9e306e1d274ca2c114d1323bf8
j4games4ev@gmail.com <j4games4ev@gmail.com>
2019-04-06 23:57:58 +0530

    Initial layout setup.

---
+++ Assets/Scripts/ScriptableObjectForChef.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;         
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjectForChef", order = 1)]
+public class ScriptableObjectForChef : ScriptableObject
+{
+   //State machines
+   public enum PLAYER
+    {
+        PLAYER_1,
+        PLAYER_2
+    }
+
+    public enum PICKUP
+    {
+        NONE,
+        SPEED,
+        TIME,
+        SCORE
+    }
+
+    //Public States
+    public PLAYER thisPlayer;
+    public PICKUP currentPickup;
+
+    //Player property variables
+    public GameObject[] pickedVegetables;
+    public GameObject pickedSalad;
+    public bool chopping;
+    public float movementSpeed;
+    public int playerTime;
+    public int playerScore;
+}
